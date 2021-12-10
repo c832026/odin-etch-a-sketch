6d7b2bd562d6696e7e40fe2d117e33b43c53f73b
c832026 <c832026@gmail.com>
2021-12-10 13:55:47 +0800

    Complete functionalities of drawing on grid and reset the grid

--- static/etch-a-sketch.js
+++ static/etch-a-sketch.js
@@ -1,11 +1,50 @@
-// Create a desired-number x desired-number of grid elements using Javascript
-// #TODO  
+// Setup global variables
+const CONTAINER = document.querySelector('.flex-container');
+const GRIDLENGTH = 600;
 
-// Add listener to each element, change their background color when hovering over
-// #TODO
+function makeGrid(squaresPerSides = 10){
+    const squareLength = GRIDLENGTH / squaresPerSides;
+    
+    // Create the grid depends on squares-per-sides value
+    for (let i = 0; i < squaresPerSides; i++) {
+        for (let j = 0; j < squaresPerSides; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            // Set each grid elements with equal height and width
+            square.style.height = `${squareLength}px`;
+            square.style.width = `${squareLength}px`;
+            // Change grid elements background color when hovering over
+            square.addEventListener('mouseenter', () => {
+                square.classList.add('square-onhover');
+            }, {once: true});
+            // Append each elements to the flex container
+            CONTAINER.appendChild(square);
+        }
+    }
+}
 
-// Attach the elements inside a flex-box container
-// #TODO
+// Prompt the new squares per side, create the new grid
+function resetGrid() {
+    // Prompt for squares per sides
+    const squarePerSides = prompt('How many squares you want per sides? // Maximum: 100' );
+    // Alert user for invalid input
+    if (squarePerSides > 100) {
+        alert('The number\'s too big! Maximum is 100');
+        return;
+    }
 
-// Reset the whole grid blackground color to default when Click the button
-// #TODO
+    // Delete the grid elements
+    while (CONTAINER.firstChild) {
+        CONTAINER.removeChild(CONTAINER.firstChild);
+    }
+    
+    // Create grid using the prompted value
+    makeGrid(squarePerSides);
+}
+
+
+// Reset the grid when click the button
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', resetGrid);
+
+makeGrid();
