4f1f81e32307a4f753244d46da7dd2a8b926345e
c832026 <c832026@gmail.com>
2021-12-10 16:54:42 +0800

    Add two buttons, one button change trace color to random, another return color to black

--- static/etch-a-sketch.js
+++ static/etch-a-sketch.js
@@ -1,7 +1,4 @@
-// Setup global variables
-const CONTAINER = document.querySelector('.flex-container');
-const GRIDLENGTH = 600;
-
+// Initialize default 10x10 grid
 function makeGrid(squaresPerSides = 10){
     const squareLength = GRIDLENGTH / squaresPerSides;
     
@@ -14,9 +11,7 @@ function makeGrid(squaresPerSides = 10){
             square.style.height = `${squareLength}px`;
             square.style.width = `${squareLength}px`;
             // Change grid elements background color when hovering over
-            square.addEventListener('mouseenter', () => {
-                square.classList.add('square-onhover');
-            }, {once: true});
+            square.addEventListener('mouseenter', setBlackColor, {once: true});
             // Append each elements to the flex container
             CONTAINER.appendChild(square);
         }
@@ -31,6 +26,8 @@ function resetGrid() {
     if (squarePerSides > 100) {
         alert('The number\'s too big! Maximum is 100');
         return;
+    } else if (!squarePerSides) {
+        return;
     }
 
     // Delete the grid elements
@@ -42,9 +39,65 @@ function resetGrid() {
     makeGrid(squarePerSides);
 }
 
+// Event listener function for change trace color to black
+function setBlackColor(event) {
+    event.target.style.backgroundColor = 'black';
+}
 
-// Reset the grid when click the button
+// Event listener function for change trace color to random color
+function setRandomColor(event) {
+    // Generate random rgb value
+    const rgb = [];
+    for (let i = 0; i < 3; i++) {
+        rgb[i] = Math.floor(Math.random() * (255 + 1));
+    }
+    // Assign random color
+    event.target.style.backgroundColor = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+}
+
+// Change trace color to black color
+function toBlackColor() {
+    // Select all the grid elements
+    const squares = document.querySelectorAll('.square');
+
+    // Remove event listener that change the color to random
+    squares.forEach((square) => {
+        square.removeEventListener('mouseenter', setRandomColor);
+
+        // Add even listener to change the color to black
+        square.addEventListener('mouseenter', setBlackColor, {once: true});
+    });
+}
+
+// Change trace color to random color
+function toRandomColor() {
+    // Select all the grid elements
+    const squares = document.querySelectorAll('.square');
+
+    // Remove event listener that change the color to black
+    squares.forEach((square) => {
+        square.removeEventListener('mouseenter', setBlackColor);
+  
+        // Add event listener to change the color to random
+        square.addEventListener('mouseenter', setRandomColor, {once: true});
+    });
+}
+
+
+// Setup global variables
+const CONTAINER = document.querySelector('.flex-container');
+const GRIDLENGTH = 600;
+
+// Reset the grid when clicked the reset-button
 const reset = document.querySelector('#reset');
 reset.addEventListener('click', resetGrid);
+
+// Change trace color to black color when clicked the black-button
+const black = document.querySelector('#black');
+black.addEventListener('click', toBlackColor);
+
+// Change trace color to random color when clicked the random-button
+const random = document.querySelector('#random');
+random.addEventListener('click', toRandomColor);
 
 makeGrid();
